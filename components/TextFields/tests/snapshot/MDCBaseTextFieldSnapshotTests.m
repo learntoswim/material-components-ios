f64fb1fac8252e5e1b51bcfc34b454feb44b389d
Andrew Overton <andrewoverton@google.com>
2019-09-06 11:06:19 -0400

    WIP

--- components/TextFields/tests/snapshot/MDCBaseTextFieldSnapshotTests.m
+++ components/TextFields/tests/snapshot/MDCBaseTextFieldSnapshotTests.m
@@ -18,21 +18,52 @@
 
 #import "MaterialTextFields+ContainedInputView.h"
 
+static const NSTimeInterval kTextFieldValidationAnimationDuration = 1.0;
+static const NSTimeInterval kTextFieldValidationAnimationTimeout = 1.5;
+
 @interface MDCBaseTextFieldTestsSnapshotTests : MDCSnapshotTestCase
+@property (strong, nonatomic) MDCBaseTextField *textField;
 @end
 
 @implementation MDCBaseTextFieldTestsSnapshotTests
 
 - (void)setUp {
+  NSLog(@"set up");
   [super setUp];
-
+  
   // Uncomment below to recreate all the goldens (or add the following line to the specific
   // test you wish to recreate the golden for).
-  //    self.recordMode = YES;
+//  self.recordMode = YES;
 }
 
 - (void)tearDown {
+  NSLog(@"tear down up");
   [super tearDown];
+}
+
+- (MDCBaseTextField *)createBaseTextFieldInKeyWindow {
+  MDCBaseTextField *textField = [[MDCBaseTextField alloc] initWithFrame:CGRectMake(0, 0, 200, 60)];
+  textField.borderStyle = UITextBorderStyleRoundedRect;
+  UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+  [keyWindow addSubview:textField];
+  return textField;
+}
+
+- (void)validateTextField:(MDCBaseTextField *)textField {
+  XCTestExpectation *expectation =
+  [[XCTestExpectation alloc] initWithDescription:@"textfield_validation_expectation"];
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+                               (int64_t)(kTextFieldValidationAnimationDuration * NSEC_PER_SEC)),
+                 dispatch_get_main_queue(), ^{
+                   // We take a snapshot of the textfield so we don't have to remove it from the app
+                   // host's key window. Removing the textfield from the app host's key window
+                   // before validation can affect the textfield's editing behavior, which has a
+                   // large effect on the appearance of the textfield.
+                   UIView *textFieldSnapshot = [textField snapshotViewAfterScreenUpdates:YES];
+                   [self generateSnapshotAndVerifyForView:textFieldSnapshot];
+                   [expectation fulfill];
+                 });
+  [self waitForExpectations:@[ expectation ] timeout:kTextFieldValidationAnimationTimeout];
 }
 
 - (UIView *)createBlueSideView {
@@ -49,12 +80,6 @@
   return view;
 }
 
-- (MDCBaseTextField *)createBaseTextField {
-  MDCBaseTextField *textField = [[MDCBaseTextField alloc] initWithFrame:CGRectMake(0, 0, 200, 60)];
-  textField.borderStyle = UITextBorderStyleRoundedRect;
-  return textField;
-}
-
 - (void)generateSnapshotAndVerifyForView:(UIView *)view {
   UIView *snapshotView = [view mdc_addToBackgroundView];
   [self snapshotVerifyView:snapshotView];
@@ -64,18 +89,18 @@
 
 - (void)testTextFieldWithText {
   // Given
-  MDCBaseTextField *textField = [self createBaseTextField];
+  MDCBaseTextField *textField = [self createBaseTextFieldInKeyWindow];
 
   // When
   textField.text = @"Text";
 
   // Then
-  [self generateSnapshotAndVerifyForView:textField];
+  [self validateTextField:textField];
 }
 
 - (void)testTextFieldWithLeadingView {
   // Given
-  MDCBaseTextField *textField = [self createBaseTextField];
+  MDCBaseTextField *textField = [self createBaseTextFieldInKeyWindow];
 
   // When
   textField.text = @"Text";
@@ -83,12 +108,12 @@
   textField.leadingViewMode = UITextFieldViewModeAlways;
 
   // Then
-  [self generateSnapshotAndVerifyForView:textField];
+  [self validateTextField:textField];
 }
 
 - (void)testTextFieldWithTrailingView {
   // Given
-  MDCBaseTextField *textField = [self createBaseTextField];
+  MDCBaseTextField *textField = [self createBaseTextFieldInKeyWindow];
 
   // When
   textField.text = @"Text";
@@ -96,12 +121,13 @@
   textField.trailingViewMode = UITextFieldViewModeAlways;
 
   // Then
-  [self generateSnapshotAndVerifyForView:textField];
+  [self validateTextField:textField];
 }
 
 - (void)testTextFieldWithLeadingViewAndTrailingView {
+  NSLog(@"testTextFieldWithLeadingViewAndTrailingView");
   // Given
-  MDCBaseTextField *textField = [self createBaseTextField];
+  MDCBaseTextField *textField = [self createBaseTextFieldInKeyWindow];
 
   // When
   textField.text = @"Text";
@@ -111,19 +137,20 @@
   textField.leadingViewMode = UITextFieldViewModeAlways;
 
   // Then
-  [self generateSnapshotAndVerifyForView:textField];
+  [self validateTextField:textField];
 }
 
 - (void)testTextFieldWithVisibleClearButton {
+  NSLog(@"testTextFieldWithVisibleClearButton");
   // Given
-  MDCBaseTextField *textField = [self createBaseTextField];
+  MDCBaseTextField *textField = [self createBaseTextFieldInKeyWindow];
 
   // When
   textField.clearButtonMode = UITextFieldViewModeAlways;
   textField.text = @"Text";
 
   // Then
-  [self generateSnapshotAndVerifyForView:textField];
+  [self validateTextField:textField];
 }
 
 @end
